4b12839a47c88e27f6d12db30a4174841c8983cd
Gabriel <gbri2586@gmail.com>
2024-11-02 21:48:08 -0300

    inicio main

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const nav = document.querySelector(".navbar")
 hamburguer.addEventListener('click', () =>
     nav.classList.toggle("active")
 )
+
+
+const text = document.querySelector(".sec-text")
+const textLoad = () => {
+    setTimeout(() =>{
+        text.textContent = "UX/UI Designer"
+    }, 0);
+    setTimeout(() =>{
+        text.textContent = "Desenvolvedor"
+    }, 4000);
+    setTimeout(() =>{
+        text.textContent = "Freelancer"
+    }, 8000);
+}
+
+textLoad();
+setInterval(textLoad, 12000)
